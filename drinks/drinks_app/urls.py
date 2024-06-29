9573c6d9bc53846b9c3427b82534660ab1bd34d7
lewmas9152 <smnjuguna22@gmail.com>
2024-06-29 07:18:01 +0300

    made use of generics and mixins

--- drinks/drinks_app/urls.py
+++ drinks/drinks_app/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('drink/', views.drinks, name= 'drinks'),
     path('drink/<int:pk>', views.drink_detail, name= 'detail'),
     path('drinkAPIView/', views.DrinksAPIView.as_view(), name= 'drinksAPIView'),
-    path('drinkAPIView/<int:pk>', views.drink_detailAPIView.as_view(), name= 'DetailAPIview')
+    path('drinkAPIView/<int:pk>', views.drink_detailAPIView.as_view(), name= 'DetailAPIview'),
+    path('drinkGenericView/', views.DrinksGenericView.as_view(), name= 'GenericView'),
+    path('drinkGenericView/<int:id>', views.DrinksGenericView.as_view(), name= 'GenericViewDetail'),
+
 ]
--- drinks/drinks_app/views.py
+++ drinks/drinks_app/views.py
@@ -5,8 +5,35 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from django.http import Http404
+from rest_framework import mixins
+from rest_framework import generics
 
+# using mixins
+class DrinksGenericView(generics.GenericAPIView,
+                        mixins.ListModelMixin,
+                        mixins.CreateModelMixin,
+                        mixins.RetrieveModelMixin,
+                        mixins.UpdateModelMixin,
+                        mixins.DestroyModelMixin):
+    queryset = Drinks.objects.all()
+    serializer_class = DrinkSerializer
+    lookup_field = 'id'
 
+    def get(self,request,id=None):
+        if id:
+            return self.retrieve(request, id=id)
+        return self.list(request)
+    
+    def post(self,request):
+        return self.create(request)
+    
+    def put(self,request, id=None):
+        return self.update(request,id=id)
+    
+    def delete(self,request,id=None):
+        return self.destroy(request, id=id)
+
+# using class based apiviews
 class DrinksAPIView(APIView):
     def get(self,request):
         drinks = Drinks.objects.all()
@@ -20,22 +47,6 @@ class DrinksAPIView(APIView):
             return Response(serializer.data, status= status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
   
-@ api_view(['GET','POST'])
-def drinks(request):
-
-    if request.method == 'GET':
-        drinks = Drinks.objects.all()
-        serializer = DrinkSerializer(drinks, many=True)
-        return Response({"drinks":serializer.data})
-    
-    if request.method == 'POST':
-        serializer = DrinkSerializer(data= request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status= status.HTTP_201_CREATED)
-        
-
-
 class drink_detailAPIView(APIView):
     def get_object(self,pk):
         try:
@@ -62,6 +73,21 @@ class drink_detailAPIView(APIView):
         drink.delete()
         return Response(status = status.HTTP_204_NO_CONTENT)
 
+# using apiview
+@ api_view(['GET','POST'])
+def drinks(request):
+
+    if request.method == 'GET':
+        drinks = Drinks.objects.all()
+        serializer = DrinkSerializer(drinks, many=True)
+        return Response({"drinks":serializer.data})
+    
+    if request.method == 'POST':
+        serializer = DrinkSerializer(data= request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status= status.HTTP_201_CREATED)
+        
 @ api_view(['GET','PUT','DELETE'])
 def drink_detail(request,pk):
     try:
