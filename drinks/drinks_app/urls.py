2c5150fe2909d056e057c74f5f75a1db768d3ec0
lewmas9152 <smnjuguna22@gmail.com>
2024-06-29 06:29:49 +0300

    made use of class based APIViews

--- drinks/drinks_app/urls.py
+++ drinks/drinks_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('drink/', views.drinks, name= 'drinks'),
-    path('drink/<int:pk>', views.drink_detail, name= 'drink')
+    path('drink/<int:pk>', views.drink_detail, name= 'detail'),
+    path('drinkAPIView/', views.DrinksAPIView.as_view(), name= 'drinksAPIView'),
+    path('drinkAPIView/<int:pk>', views.drink_detailAPIView.as_view(), name= 'DetailAPIview')
 ]
--- drinks/drinks_app/views.py
+++ drinks/drinks_app/views.py
@@ -1,13 +1,25 @@
 from .models import Drinks
 from .serializers import DrinkSerializer
-from django.http import JsonResponse, HttpResponse
-from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from django.views.decorators.csrf import csrf_exempt
+from rest_framework.views import APIView
+from django.http import Http404
 
 
+class DrinksAPIView(APIView):
+    def get(self,request):
+        drinks = Drinks.objects.all()
+        serializer = DrinkSerializer(drinks, many=True)
+        return Response({"drinks":serializer.data})
+    
+    def post(self,request):
+        serializer = DrinkSerializer(data= request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status= status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+  
 @ api_view(['GET','POST'])
 def drinks(request):
 
@@ -21,20 +33,47 @@ def drinks(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status= status.HTTP_201_CREATED)
+        
+
+
+class drink_detailAPIView(APIView):
+    def get_object(self,pk):
+        try:
+            return Drinks.objects.get(pk=pk)
+        except Drinks.DoesNotExist:
+            raise Http404
+    
+    def get(self,request,pk):
+        drink = self.get_object(pk)
+        serializer = DrinkSerializer(drink)
+        return Response(serializer.data)
+    
+    def put(self,request,pk):
+        drink = self.get_object(pk)
+        serializer = DrinkSerializer(drink, data = request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status= status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self,request,pk):
+        drink = self.get_object(pk)
+        drink.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
 
 @ api_view(['GET','PUT','DELETE'])
 def drink_detail(request,pk):
     try:
         drink = Drinks.objects.get(pk=pk)
     except Drinks.DoesNotExist:
-        return HttpResponse(status = status.HTTP_404_NOT_FOUND)
+        return Response(status = status.HTTP_404_NOT_FOUND)
     if request.method == "GET":
         serializer = DrinkSerializer(drink)
         return Response(serializer.data)
     
     if request.method == "PUT":
-        data = JSONParser().parse(request)
-        serializer = DrinkSerializer(data)
+        serializer = DrinkSerializer(data = request.data)
 
         if serializer.is_valid():
             serializer.save()
@@ -42,4 +81,4 @@ def drink_detail(request,pk):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     if request.method == "DELETE" :
         drink.delete()
-        return HttpResponse(status = status.HTTP_204_NO_CONTENT)
+        return Response(status = status.HTTP_204_NO_CONTENT)
